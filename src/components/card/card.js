faaf2687b9c42a080e13d63a72b9b2a73d200803
Aleksandra <shurup4ik1993@gmail.com>
2020-11-06 03:12:59 +0100

    first commit

---
+++ src/components/card/card.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import defaultImage from '../../images/default.jpg'
+import report from '../../images/report.svg'
+import menu from '../../images/menu.svg'
+import vcr from '../../images/vcr.svg'
+import impression from '../../images/impression.svg'
+import './card.css'
+
+export const STATUS_RUNNING = "RUNNING";
+export const STATUS_PAUSED = "PAUSED";
+export const STATUS_STOPPED = "STOPPED";
+
+function Card(props) {
+    const {
+        image,
+        status,
+        nameOfCompany,
+        advertiser,
+        compaignDates,
+        budgetTotal,
+        budgetSpent,
+        averageCPM,
+        currentVCR,
+        uniqueReach,
+        frequency,
+        totalImpressions
+    } = props;
+
+    const renderStatus = () => {
+        if (status == STATUS_RUNNING) {
+            return (
+                <div className="status running">
+                    <span>Running</span>
+                </div>
+            );
+        } else if (status == STATUS_PAUSED) {
+            return (
+                <div className="status paused">
+                    <span>Paused</span>
+                </div>
+            );
+        } else if (status == STATUS_STOPPED) {
+            return (
+                <div className="status stopped">
+                    <span>Stopped</span>
+                </div>
+            );
+        } else {
+            return (
+                <div className="status">
+                    <span>None</span>
+                </div>
+            );
+        }
+    };
+
+    return (
+
+        <div className="card">
+            <div className="img">
+                <img src={image ?? defaultImage} alt="img Of company" />
+            </div>
+            <div className="content">
+                <div className="compamyProps">
+                    {renderStatus()}
+                    <div className="nameOfCompany">
+                        <span>{nameOfCompany ?? "–"}</span>
+                    </div>
+                    <div className="nav">
+                        <a href="#">
+                            <h2>Report</h2>
+                            <div className="reportIcon">
+                                <img src={report} alt="report" />
+                            </div>
+                        </a>
+                        <div className="menu">
+                            <a href="#">
+                                <img src={menu} alt="menu" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="img">
+                        <img src={image ?? defaultImage} alt="img Of company" />
+                    </div>
+                </div>
+
+                <div className="props">
+                    {renderStatus()}
+                    <div className="advert end">
+                        <h3>Advertiser</h3>
+                        <span>{advertiser ?? "–"}</span>
+                    </div>
+                    <hr className="border"></hr>
+                    <div className="compaignDates">
+                        <h3>Campaign dates (7 days left)</h3>
+                        <span>{compaignDates ?? "–"}</span>
+                    </div>
+                    <div className="budget end">
+                        <h3>Budget</h3>
+                        <span>{budgetTotal ? `\$${budgetTotal}` : "–"}</span>
+                    </div>
+                    <div className="spent">
+                        <h3>Spent</h3>
+                        <span>{budgetSpent ? `\$${budgetSpent}` : "–"}</span>
+                    </div>
+                    <div className="cpm">
+                        <h3>Average CPM </h3>
+                        <span>{averageCPM ? `\$${averageCPM}` : "–"}</span>
+                    </div>
+                    <div className="vcr end">
+                        <h3>Current VCR</h3>
+                        <div>
+                            <span>{currentVCR ? `${currentVCR}%` : "–"}</span>
+                            <img src={currentVCR ? `${vcr}` : " "} alt="" />
+                        </div>
+                    </div>
+                    <div className="uniq">
+                        <h3>Unique reach</h3>
+                        <div>
+                            <span>{uniqueReach ?? "–"}</span>
+                            <img src={uniqueReach ? `${impression}` : " "} alt="" />
+                        </div>
+                    </div>
+                    <div className="frequency">
+                        <h3>Frequency</h3>
+                        <span>{frequency ?? "–"}</span>
+                    </div>
+                    <div className="impressions end">
+                        <h3>Total Impressions</h3>
+                        <div>
+                            <span>{totalImpressions ?? "–"}</span>
+                            <img src={uniqueReach ? `${impression}` : " "} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Card;
---
+++ src/components/card/card.stories.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Card, {STATUS_RUNNING, STATUS_PAUSED, STATUS_STOPPED} from './card'
+import img from '../../images/Creative.png'
+
+export default {
+    title: 'Card',
+    component: Card
+}
+
+const loadedProps = {
+    image: img,
+    nameOfCompany: 'Premium_Fit_1507114822_Mar_Bowl',
+    compaignDates: '05 Mar 2020 - 05 Apr 2020',
+    advertiser: 'Planet_Fitness',
+    budgetTotal: 800,
+    budgetSpent: 500,
+    averageCPM: 55,
+    currentVCR: 20,
+    uniqueReach: 10000,
+    frequency: 6,
+    totalImpressions: 5000
+}
+
+const unloadedProps = {
+    nameOfCompany: 'Premium_Fit_1507114822_Mar_Bowl',
+    compaignDates: '05 Mar 2020 - 05 Apr 2020',
+    advertiser: 'Planet_Fitness',
+    budgetTotal: 800,
+}
+
+
+export const LoadedRunning = () => <Card {...loadedProps} status={STATUS_RUNNING} />
+export const LoadedPaused = () => <Card {...loadedProps} status={STATUS_PAUSED}/>
+export const LoadedStoped = () => <Card {...loadedProps} status={STATUS_STOPPED} />
+export const NoDataRunning = () => <Card {...unloadedProps} status={STATUS_RUNNING}/>
+export const NoDataPaused= () => <Card {...unloadedProps} status={STATUS_PAUSED}/>
+export const NoDataStopped = () => <Card {...unloadedProps} status={STATUS_STOPPED} />
